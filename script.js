df3a2f94a65120451502d7d4f724945feed7ceee
Rain Wang <runyu@umail.ucsb.edu>
2020-04-20 23:50:18 -0700

    week2

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function openModal(){
+  document.getElementById("modal").style.display = "block"
+}
+
+// function closeModal() {
+//   document.getElementById("modal").style.display = "none"
+// }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+{/* // Next/previous controls */}
+function plusSlides(n) {
+  showSlides(slideIndex += n)
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  // console.log("Here2");
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
